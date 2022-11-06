964b83be1fc43c6657cb0c2181bd7e352fa81fa6
DergachovaAnna <ann.dergachova@gmail.com>
2022-11-06 21:24:32 +0100

    dashboard / match form updates

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,4 +2,26 @@ from pages.base_page import BasePage
 
 
 class Dashboard(BasePage):
-    one_xpath = "//*[@id='login']"
+    main_page_menu_xpath = "//ul[1]/div[1]/span"
+    players_menu_xpath = "//ul[1]/div[2]/span"
+    language_menu_xpath = "//ul[2]/div[1]"
+    sign_out_menu_xpath = "//ul[2]/div[2]"
+    block_players_xpath = "//main//div[1][contains(@class, 'MuiGrid-grid-xs-6')]"
+    block_matches_xpath = "//main//div[2][contains(@class, 'MuiGrid-grid-xs-6')]"
+    block_reports_xpath = "//main//div[3][contains(@class, 'MuiGrid-grid-xs-6')]"
+    block_events_xpath = "//main//div[4][contains(@class, 'MuiGrid-grid-xs-6')]"
+    add_player_xpath = "//a[contains(@href, '/en/players/add')]"
+    dev_contact_xpath = "//a[contains(@href, 'https://app.slack.com/client/T3X4CAKNU/C3XTEGXB6')]"
+    logo_xpath = "//main/div[3]/div[1]/div/div[1]"
+    main_header_xpath = "//main//h2[text()='Scouts Panel']"
+    main_panel_text_xpath = "//main//p"
+    last_created_player_xpath = "//a[1][contains(@href, 'edit')]"
+    last_updated_player_xpath = "//a[2][contains(@href, 'edit')]"
+    last_created_match_xpath = "//a[3][contains(@href, 'match')]"
+    last_updated_match_xpath = "//a[4][contains(@href, 'match')]"
+    last_updated_report_xpath = "//a[5][contains(@href, 'reports')]"
+
+
+
+
+
--- pages/match_form.py
+++ pages/match_form.py
@@ -2,4 +2,40 @@ from pages.base_page import BasePage
 
 
 class matchForm(BasePage):
-    login_xpath = "//*[@id='login']"
+    main_page_menu_xpath = "",
+    players_menu_xpath = "",
+    player_menu_xpath = "",
+    matches_menu_xpath = "",
+    reports_menu_xpath = "",
+    language_menu_xpath = "",
+    sign_out_menu_xpath = "",
+    page_title_xpath = "",
+    add_language_button_xpath = "",
+    add_link_button_xpath = "",
+    submit_button_xpath = "",
+    clear_button_xpath = "",
+    email_input_xpath = "",
+    name_input_xpath = "",
+    surname_input_xpath = "",
+    phone_input_xpath = "",
+    weight_select_xpath = "",
+    height_select_xpath = "",
+    age_select_xpath = "",
+    leg_select_xpath = "",
+    club_input_xpath = "",
+    level_input_xpath = "",
+    main_position_input_xpath = "",
+    second_position_input_xpath = "",
+    district_select_xpath = "",
+    achievements_input_xpath = "",
+    language_input_xpath = "",
+    remove_language_xpath = "",
+    youtube_input_xpath = "",
+    remove_youtube_xpath = "",
+    ball_input_xpath = "",
+    minut_90_input_xpath = "",
+    facebook_input_xpath = "",
+
+
+
+
