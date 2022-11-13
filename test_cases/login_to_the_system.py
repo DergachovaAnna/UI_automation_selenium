940b284839e66bfb46abf09cdd96d3661d53789d
DergachovaAnna <ann.dergachova@gmail.com>
2022-11-14 00:44:32 +0100

    update

---
+++ test_cases/login_to_the_system.py
@@ -0,0 +1,36 @@
+import os
+import unittest
+from selenium import webdriver
+
+from pages.dashboard import Dashboard
+from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+from pages.login_page import LoginPage
+import time
+
+
+class TestLoginPage(unittest.TestCase):
+
+    driver = None
+
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_login_to_system(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_the_page()
+        user_login.type_in_email('user01@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.click_button()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_the_page()
+        time.sleep(5)
+
