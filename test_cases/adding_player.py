e69c7dc5d45bb5bd5ccb56b057c58a51e176d5ea
DergachovaAnna <ann.dergachova@gmail.com>
2022-11-17 23:23:27 +0100

    update after lesson review

--- test_cases/adding_player.py
+++ test_cases/adding_player.py
@@ -3,7 +3,6 @@ import unittest
 from selenium import webdriver
 
 from pages.add_a_player import AddPlayer
-from pages.base_page import BasePage
 from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
 from pages.login_page import LoginPage
 from pages.dashboard import Dashboard
@@ -21,46 +20,41 @@ class TestAddPlayer(unittest.TestCase):
         self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
         self.driver.fullscreen_window()
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
+        self.login_page = LoginPage(self.driver)
+        self.add_player = AddPlayer(self.driver)
 
-    @classmethod
-    def tearDown(self):
-        self.driver.quit()
-
-
+    """Filling in form for new player"""
     def test_add_player(self):
-        user_login = LoginPage(self.driver)
-        user_login.type_in_email("user01@getnada.com")
-        user_login.type_in_password("Test-1234")
-        user_login.click_button()
+        self.login_page.type_in_email("user01@getnada.com")
+        self.login_page.type_in_password("Test-1234")
+        self.login_page.click_button()
         time.sleep(5)
         dashboard_page = Dashboard(self.driver)
         dashboard_page.click_button_add_player()
         time.sleep(3)
-        add_player = AddPlayer(self.driver)
-        add_player.type_in_email("anna_QA123@gmail.com")
-        add_player.input_name("Harry")
-        add_player.input_surname("Potter")
-        add_player.input_age("10.09.1993")
-        add_player.input_leg("12")
-        add_player.input_club("Griffindor")
-        add_player.input_height("179")
-        add_player.input_weight("123")
-        add_player.input_phone("0991071113")
-        add_player.input_level("9")
-        add_player.click_button()
+        self.add_player.type_in_email("anna_QA123@gmail.com")
+        self.add_player.input_name("Harry")
+        self.add_player.input_surname("Potter")
+        self.add_player.input_age("10.09.1993")
+        self.add_player.input_leg("12")
+        self.add_player.input_club("Griffindor")
+        self.add_player.input_height("179")
+        self.add_player.input_weight("123")
+        self.add_player.input_phone("0991071113")
+        self.add_player.input_level("9")
+        self.add_player.click_button()
         time.sleep(2)
-        add_player.select_option()
-        add_player.input_main_position("Wizard")
-        add_player.click_button_submit()
+        self.add_player.select_option()
+        self.add_player.input_main_position("Wizard")
+        self.add_player.click_button_submit()
         time.sleep(6)
 
-
+    """Check header is correct"""
     def test_title_add_player(self):
-        user_login = LoginPage(self.driver)
-        user_login.type_in_email("user01@getnada.com")
-        user_login.type_in_password("Test-1234")
-        user_login.click_button()
-        time.sleep(5)
+        self.login_page.type_in_email("user01@getnada.com")
+        self.login_page.type_in_password("Test-1234")
+        self.login_page.click_button()
+        time.sleep(2)
         dashboard_page = Dashboard(self.driver)
         dashboard_page.click_button_add_player()
         time.sleep(2)
@@ -70,4 +64,8 @@ class TestAddPlayer(unittest.TestCase):
 
     def get_page_title(self, url):
         return self.driver.title
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
 
--- test_cases/login_to_the_system.py
+++ test_cases/login_to_the_system.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from pages.dashboard import Dashboard
 from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
 from pages.login_page import LoginPage
-import time
 
 
 class TestLoginPage(unittest.TestCase):
@@ -19,21 +18,21 @@ class TestLoginPage(unittest.TestCase):
         self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
         self.driver.fullscreen_window()
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
+        self.login_page = LoginPage(self.driver)
+
+    """Logging in to the system"""
+    def test_login_to_system(self):
+        self.login_page.title_of_the_page()
+        self.login_page.type_in_email('user01@getnada.com')
+        self.login_page.type_in_password('Test-1234')
+        self.login_page.click_button()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_the_page()
+
+    """Check header by text"""
+    def test_check_by_text(self):
+        self.login_page.assert_element_text(self.driver, self.login_page.header_text_xpath, expected_text='Scouts Panel')
 
     @classmethod
     def tearDown(self):
         self.driver.quit()
-
-    def test_login_to_system(self):
-        user_login = LoginPage(self.driver)
-        user_login.title_of_the_page()
-        user_login.type_in_email('user01@getnada.com')
-        user_login.type_in_password('Test-1234')
-        user_login.click_button()
-        dashboard_page = Dashboard(self.driver)
-        dashboard_page.title_of_the_page()
-        time.sleep(5)
-
-    def test_check_by_text(self):
-        user_login = LoginPage(self.driver)
-        user_login.assert_element_text(self.driver, xpath='//*[text()="Scouts Panel"]', expected_text="Scouts Panel")
